283b23d537a29264ac1f2f30f4d7fa7b3735112c
BuildTools <unconfigured@null.spigotmc.org>
2021-09-25 20:37:47 -0400

    ccccccccc

--- src/main/java/com/jasonjat/testingmod/screen/Gui.java
+++ src/main/java/com/jasonjat/testingmod/screen/Gui.java
@@ -19,7 +19,7 @@ public class Gui extends LightweightGuiDescription {
         });
 
         WButton button2 = new WButton(new LiteralText("Hi guys"));
-        root.add(button2, 20,20,20,20);
+        root.add(button2, 0,2,20,20);
 
         button2.setOnClick(() -> {
             System.out.println("Client only, but server if used packets.");
