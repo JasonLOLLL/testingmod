876959351a9540cb0a2ba68612d80440d3c0efee
JasonLOLLL <jasonli2068@gmail.com>
2021-09-30 11:45:36 -0400

    trying out making mob entities, made a penguin entity that is not textured

--- src/main/java/com/jasonjat/testingmod/client/Keybinds.java
+++ src/main/java/com/jasonjat/testingmod/client/Keybinds.java
@@ -7,9 +7,11 @@ import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.sound.SoundEvents;
 import org.lwjgl.glfw.GLFW;
 
 @Environment(EnvType.CLIENT)
@@ -28,6 +30,8 @@ public class Keybinds {
                 PacketByteBuf packetByteBuf = new PacketByteBuf(Unpooled.buffer());
                 packetByteBuf.writeString("C");
                 ClientPlayNetworking.send(ModPackets.KEYBIND_PACKET, packetByteBuf);
+
+                MinecraftClient.getInstance().player.playSound(SoundEvents.ENTITY_DRAGON_FIREBALL_EXPLODE, 1f, 1f);
             }
         });
     }
--- src/main/java/com/jasonjat/testingmod/modpackets/ModPacketsC2S.java
+++ src/main/java/com/jasonjat/testingmod/modpackets/ModPacketsC2S.java
@@ -2,7 +2,6 @@ package com.jasonjat.testingmod.modpackets;
 
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -10,6 +9,7 @@ import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundEvents;
 
 public class ModPacketsC2S {
 
@@ -21,14 +21,9 @@ public class ModPacketsC2S {
     private static void keybind(MinecraftServer minecraftServer, ServerPlayerEntity serverPlayerEntity, ServerPlayNetworkHandler serverPlayNetworkHandler, PacketByteBuf packetByteBuf, PacketSender packetSender) {
         String message = packetByteBuf.readString();
         if (message.equals("C")) {
-            serverPlayerEntity.setOnFireFor(1);
+            PlayerEntity player = (PlayerEntity) serverPlayerEntity;
+            player.playSound(SoundEvents.ENTITY_DRAGON_FIREBALL_EXPLODE, 1f, 1f);
         }
-    }
-
-    private static void spawnArrow(MinecraftServer minecraftServer, ServerPlayerEntity serverPlayerEntity, ServerPlayNetworkHandler serverPlayNetworkHandler, PacketByteBuf packetByteBuf, PacketSender packetSender) {
-        Entity entity = null;
-
-
     }
 
     private static void guiThing(MinecraftServer minecraftServer, ServerPlayerEntity serverPlayerEntity, ServerPlayNetworkHandler serverPlayNetworkHandler, PacketByteBuf packetByteBuf, PacketSender packetSender) {
@@ -40,6 +35,4 @@ public class ModPacketsC2S {
             player.kill();
         }
     }
-
-
 }
