123fe866f21adfb8f6a84c7d40e67eaf4024d758
JasonLOLLL <jasonli2068@gmail.com>
2021-10-22 18:38:18 -0400

    GUI OVERLAY CMON WORK PLS

--- src/main/java/com/jasonjat/testingmod/screen/TestScreen.java
+++ src/main/java/com/jasonjat/testingmod/screen/TestScreen.java
@@ -1,6 +1,5 @@
 package com.jasonjat.testingmod.screen;
 
-import com.jasonjat.testingmod.Testingmod;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -28,13 +27,41 @@ public class TestScreen extends Screen {
         addDrawableChild(new ButtonWidget(250,100,50,20, Text.of("Button"), b -> {
             System.out.println("HELLO! BUTTON!");
         }));
+        System.out.println("123");
     }
 
     @Override
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        renderBackground(matrices);
+        drawCenteredText(matrices, textRenderer, Text.of("LOLNOOB"), 65, 15, 0xFFFFFF);
         super.render(matrices, mouseX, mouseY, delta);
-        this.renderBackground(matrices);
+        renderTestScreenBackground(matrices);
     }
+
+    private void renderTestScreenBackground(MatrixStack matrices) {
+        RenderSystem.enableBlend();
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.setShaderTexture(0, WINDOW);
+        
+
+        this.drawTexture(matrices, (this.width-this.backgroundWidth)/2,(this.height-this.backgroundHeight)/2,0,0, this.backgroundWidth,this.backgroundHeight);
+        RenderSystem.disableBlend();
+    }
+
+//    @Override
+//    public void renderBackground(MatrixStack matrices, int vOffset) {
+//        super.renderBackground(matrices, vOffset);
+//        RenderSystem.enableBlend();
+//        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+//        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+//        RenderSystem.setShaderTexture(0, WINDOW);
+//
+//        this.drawTexture(matrices, (this.width-this.backgroundWidth)/2,(this.height-this.backgroundHeight)/2,0,0, this.backgroundWidth,this.backgroundHeight);
+//        RenderSystem.disableBlend();
+//    }
+
+
 
     @Override
     public boolean shouldCloseOnEsc() {
@@ -44,17 +71,5 @@ public class TestScreen extends Screen {
     @Override
     public boolean isPauseScreen() {
         return false;
-    }
-
-    @Override
-    public void renderBackground(MatrixStack matrices, int vOffset) {
-        super.renderBackground(matrices, vOffset);
-        RenderSystem.enableBlend();
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        RenderSystem.setShaderTexture(0, WINDOW);
-
-        this.drawTexture(matrices, (this.width-this.backgroundWidth)/2,(this.height-this.backgroundHeight)/2,0,0, this.backgroundWidth,this.backgroundHeight);
-        RenderSystem.disableBlend();
     }
 }
